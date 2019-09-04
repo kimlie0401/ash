9bbb46043e024f44080f43c3b179c2262d509db2
kimlie0401 <54561489+kimlie0401@users.noreply.github.com>
2019-09-04 12:51:30 -0700

    “Initial”

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import SplitText from "react-pose-text";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const charPoses1 = {
+  exit: { y: 20, opacity: 0 },
+  enter: {
+    y: 0,
+    opacity: 1,
+    transition: ({ charInWordIndex }) => ({
+      type: "spring",
+      delay: charInWordIndex * 200,
+      stiffness: 500 + charInWordIndex * 150,
+      damping: 10 - charInWordIndex * 1
+    })
+  }
+};
+
+const charPoses2 = {
+  exit: { opacity: 0, y: 20 },
+  enter: {
+    opacity: 1,
+    y: 0,
+    delay: ({ charIndex }) => charIndex * 150
+  }
+};
+
+class App extends React.Component {
+  state = {
+    isLoading: true
+  };
+
+  componentDidMount() {}
+
+  render() {
+    const { isLoading } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <div>
+              <SplitText
+                className="loader__title"
+                initialPose="exit"
+                pose="enter"
+                charPoses={charPoses1}
+              >
+                Ash
+              </SplitText>
+            </div>
+            <div>
+              <SplitText
+                className="loader__sub"
+                initialPose="exit"
+                pose="enter"
+                charPoses={charPoses2}
+              >
+                Appraisal Company
+              </SplitText>
+            </div>
+          </div>
+        ) : null}
+      </section>
+    );
+  }
 }
 
 export default App;
